92576535dd00f7107761d8633d746420bad7ad29
BSH <bruce@redefinedprop.com>
2018-07-03 23:36:14 +0000

    add if statement for env

---
+++ issuelog/test_view.py
@@ -0,0 +1,21 @@
+import unittest
+from django.test import Client, TestCase
+from .models import Issue, Comment
+
+# class GetIssueTest(unittest.TestCase):
+#     def setUp(self):
+#         # Every test needs a client.
+#         self.client = Client()
+
+#     def test_details(self):
+#         # Issue a GET request.
+#         response = self.client.get('/issue')
+
+#         # Check that the response is 200 OK.
+#         self.assertEqual(response.status_code, 200)
+
+class ViewTests(TestCase):
+    def test1(self):
+        issues = Issue.objects.exclude(issue_status = "closed").order_by('-published_date')
+        self.assertIs(issues.count(), 10)
+        
