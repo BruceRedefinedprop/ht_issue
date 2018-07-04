3def94efc8df2039958d493362b89212a4f3cd8d
BSH <bruce@redefinedprop.com>
2018-07-04 00:54:48 +0000

    delete test db

--- ht_issue/settings.py
+++ ht_issue/settings.py
@@ -14,8 +14,8 @@ import os
 # import env
 import dj_database_url
 
-# if os.path.exists('env.py'):     
-#      import env
+if os.path.exists('env.py'):     
+     import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,8 +95,8 @@ WSGI_APPLICATION = 'ht_issue.wsgi.application'
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-    TEST_DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('TEST_DATABASE_URL'))}
+    # TEST_DATABASES = {
+    # 'default': dj_database_url.parse(os.environ.get('TEST_DATABASE_URL'))}
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
